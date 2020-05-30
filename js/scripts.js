7df5da7d7f41922f8e1ba68e6bd10ff97cc97318
lthuita <lthuita@gmail.com>
2020-05-30 12:27:32 +0300

    this commits the newly declared variables - name, gender and date of birth, whose values will be extracted from the html form

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,26 @@ function weekDay(dayNum) {
   return Date.test[dayNum.getDay()];
 }
 
-function naming(){}
+function naming(){
+var yourName = document.forms["testForm"]["name"].value;
+while (yourName == "") {
+    alert("Name can't be left blank")
+  }
+
+var yourGender = document.forms["testForm"]["gender"].value;
+while (youGender == "")  {
+    alert("Please select your gender")
+
+  }
+
+var yourBirthDay = document.forms["testForm"]["birthDay"].value;
+while (birthDay.split('-').length !== 3 ||
+birthDay.split('-')[1].length !== 2 ||
+birthDay.split('-')[1] > 12 ||
+birthDay.split('-')[1] <= 0 ||
+birthDay.split('-')[2].length !== 2 ||
+birthDay.split('-')[2] > 31 ||
+birthDay.split('-')[2] <= 0) {
+alert("Invalid Date, Please enter your birthday in the format YYYY-MM-DD")
+}
+}
