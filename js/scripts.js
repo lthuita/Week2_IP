05662fd3ac087dddbc747b0ec46328c264a3cfb6
lthuita <lthuita@gmail.com>
2020-05-30 12:01:45 +0300

    declared date and names arrays variables unde my js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
+Date.test = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+var akanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Ywa', 'Kofi', 'Kwame', 'Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
