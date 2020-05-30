b01465139d16863442d2dfee4271e48d935c7a87
lthuita <lthuita@gmail.com>
2020-05-30 12:05:09 +0300

    created my first function whose variable is the number of the day of the week. Once we enter the date of birth, this is the function that will be returning the number of the day of the week (0 to 6) and then this will be aligned with the date.test array that have all the 7 days of the week in the order. Day0 of the week will correspond to the value 0 in the date.test array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 Date.test = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 var akanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Ywa', 'Kofi', 'Kwame', 'Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+
+function weekDay(dayNum) {
+  return Date.test[dayNum.getDay()];
